bc36077a09a65545db2ae266ea3809e480eb89a7
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2022-10-29 20:44:47 +0200

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Hamza Osama on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
