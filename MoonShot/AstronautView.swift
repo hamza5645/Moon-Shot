ac7968d431e88a0262d1fe4c8ea8c1c6453376e3
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2022-12-09 19:09:46 +0200

    finishing up with 1 last view

---
+++ MoonShot/AstronautView.swift
@@ -0,0 +1,36 @@
+//
+//  AstronautView.swift
+//  MoonShot
+//
+//  Created by Hamza Osama on 09/12/2022.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronaut.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+    }
+}
